4a744d7718ed61a397f427987ba87ca5ce174992
Karl Kedrovsky <karl.kedrovsky@vmlyr.com>
2019-04-17 18:33:20 -0500

    Cleaned up theme init command and removed swig

--- src/Command/InitTheme.php
+++ src/Command/InitTheme.php
@@ -10,15 +10,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class InitTheme extends DocksalCommand {
 
-  protected $config = NULL;
-
-  protected function getConfig() {
-    if (is_null($this->config)) {
-      $this->config = Yaml::parse(file_get_contents($_ENV['PROJECT_ROOT'] . '/.docksal/configuration.swig.yml'));
-    }
-    return $this->config;
-  }
-
   protected function themes() {
     return [
       'skeleto' => [
@@ -58,28 +49,6 @@ class InitTheme extends DocksalCommand {
 
     // Get config.
     $repo_config = $this->themes();
-    $swig_config = $this->getConfig();
-
-    // Get available sources.
-    $swig_sources = [];
-    if (isset($swig_config['themes']) && is_array($swig_config['themes'])) {
-      foreach ($swig_config['themes'] as $key => $theme) {
-        $swig_sources[$key] = $theme['name'];
-      }
-    }
-
-    // Exit early if there are no places to put the theme.
-    if (empty($swig_sources)) {
-      $this->message->error('No theme source directories set up.');
-      return;
-    }
-
-    // Ask for the theme source option if there is more than 1.
-    $swig_source_key = 0;
-    if (count($swig_sources) > 1) {
-      $swig_source_selection = $this->message->choice('Where would you like to build to?', $swig_sources);
-      $swig_source_key = array_search($swig_source_selection, $swig_sources);
-    }
 
     // Get requested theme to scaffold from.
     $theme_options = [];
@@ -99,62 +68,37 @@ class InitTheme extends DocksalCommand {
     ]);
 
     // Set up variables.
-    $dir_source = $swig_config['themes'][$swig_source_key]['path'];
-    $dir_themes = "{$_ENV['PROJECT_ROOT']}/{$_ENV['DOCROOT']}/themes";
-    $dir_themes_custom = "{$dir_themes}/custom";
+    $dir_source = "{$_ENV['PROJECT_ROOT']}/source/custom";
+    $dir_themes = "{$_ENV['PROJECT_ROOT']}/{$_ENV['DOCROOT']}/themes/custom";
 
     $this->message->step_header('Validating directories');
 
-    // Change to project directory.
-    chdir($_ENV['PROJECT_ROOT']);
-
-    // Validate theme directories.
-    $this->message->step_status('Drupal themes');
-    if (!is_dir($dir_themes)) {
-      $this->message->error("Theme directory doesn't exist.");
-      return;
-    }
-    $this->message->step_finish();
-
-    // Create custom folder if it doesn't exist.
+    // Create custom theme folder if it doesn't exist.
     $this->message->step_status('Drupal themes custom');
-    chdir($dir_themes);
-    if (!is_dir('custom')) {
-      mkdir('custom', 755);
+    if (!is_dir($dir_themes)) {
+      mkdir($dir_themes, 0755, TRUE);
     }
-    chdir($dir_themes_custom);
     $this->message->step_finish();
 
     // Validate destination theme directory doesn't already exist.
     $this->message->step_status('Requested theme');
+    chdir($dir_themes);
     if (is_dir($name_cleaned)) {
       $this->message->error("{$name_cleaned} theme already exists in themes/custom");
       return;
     }
     $this->message->step_finish();
 
-    // Change to project directory.
-    chdir($_ENV['PROJECT_ROOT']);
-
-    // Validate that the source directory exists.
-    $this->message->step_status('Source');
-    if (!is_dir($dir_source)) {
-      $this->message->error("Source directory \"{$dir_source}\" doesn't exist.");
-      return;
-    }
-    chdir($dir_source);
-    $this->message->step_finish();
-
     // Create themes directory if it doesn't exist.
     $this->message->step_status('Source themes');
-    if (!is_dir('themes')) {
-      mkdir('themes', 755);
+    if (!is_dir($dir_source)) {
+      mkdir($dir_source, 0755, TRUE);
     }
-    chdir('themes');
     $this->message->step_finish();
 
     // Exit early if the directory already exists.
     $this->message->step_status('Requested source');
+    chdir($dir_source);
     if (is_dir($name_cleaned)) {
       $this->message->error("{$name_cleaned} source directory already exists");
       return;
@@ -163,7 +107,7 @@ class InitTheme extends DocksalCommand {
 
     // Create theme.
     $this->message->step_header('Creating theme');
-    chdir($dir_themes_custom);
+    chdir($dir_themes);
 
     // Get theme relevant variables.
     $repo_address = $repo_config[$theme_option_key]['theme_repo'];
@@ -187,7 +131,7 @@ class InitTheme extends DocksalCommand {
 
     // Create theme source.
     $this->message->step_header('Creating source');
-    chdir("{$_ENV['PROJECT_ROOT']}/{$dir_source}/themes");
+    chdir($dir_source);
     $source_address = $repo_config[$theme_option_key]['source_repo'];
     $source_branch = $repo_config[$theme_option_key]['source_repo_branch'];
 
@@ -204,11 +148,6 @@ class InitTheme extends DocksalCommand {
     $this->runner('rm -rf README.md');
     $this->runner("find . -type f -exec sed -i 's/{$theme_option_key}/{$name_cleaned}/g' {} +");
     $this->runner("rename 's/{$theme_option_key}/{$name_cleaned}/g' *");
-    $this->message->step_finish();
-
-    // Move gulp config.
-    $this->message->step_status('Move gulp config');
-    $this->runner("mv theme_{$name_cleaned}.js ../../gulp_config/");
     $this->message->step_finish();
 
     $this->message->success("Theme created! Please update Title and Description in the theme's {$name_cleaned}.info.yml file.");
